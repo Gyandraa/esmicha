c1cd879e30988bd6eb06ed8ba610d0a42135e522
gyandra <gyandranaupal14@gmail.com>
2025-05-30 20:31:58 +0700

    update menu esmicha

---
+++ script.js
@@ -0,0 +1,234 @@
+const makanan = [
+  {
+    nama: "Nasi Ayam Katsu ",
+    harga: "Rp 15.000",
+    gambar: "images/nasiayamkatsu.jpg",
+  },
+  {
+    nama: "Ayam Katsu (Tanpa Nasi)",
+    harga: "Rp 10.000",
+    gambar: "images/nasiayamkatsu.jpg",
+  },
+  {
+    nama: "Nasi Ayam Suir",
+    harga: "Rp 15.000",
+    gambar: "images/nasiayamsuir.jpg",
+  },
+  {
+    nama: "Nasi Ayam Goreng",
+    harga: "Rp 15.000",
+    gambar: "images/nasiayamgoreng.jpg",
+  },
+  {
+    nama: "Ayam Goreng (Tanpa Nasi)",
+    harga: "Rp 10.000",
+    gambar: "images/nasiayamgoreng.jpg",
+  },
+  {
+    nama: "Paket Nasi Telur Dadar",
+    harga: "Rp 10.000",
+    gambar: "images/nasitelur.jpg",
+  },
+  {
+    nama: "Ceker Merecon + Nasi",
+    harga: "Rp 15.000",
+    gambar: "images/cekermerecon.jpg",
+  },
+  {
+    nama: "Ceker Merecon (Tanpa Nasi)",
+    harga: "Rp 10.000",
+    gambar: "images/cekermerecon.jpg",
+  },
+];
+
+const minuman = [
+  {
+    nama: "Es Avocado Coklat",
+    harga: "Rp 10.000",
+    gambar: "images/esavocado.jpg",
+  },
+  {
+    nama: "Es Cappucino Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/escappucino.jpg",
+  },
+  {
+    nama: "Es Classic Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esclassic.jpg",
+  },
+  {
+    nama: "Es Coconut Blue Soda Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/escoconutblue.jpg",
+  },
+  {
+    nama: "Es Coffee Americano Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/eskopiamericano.jpg",
+  },
+  {
+    nama: "Es Coklat Kecil Dan Besar ",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/escoklat.jpg",
+  },
+  {
+    nama: "Es Dragon Fruit Milkshake",
+    harga: "Rp 10.000",
+    gambar: "images/esdragonfruit.jpg",
+  },
+  {
+    nama: "Es Jelly Ball Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esjelyball.jpg",
+  },
+  {
+    nama: "Es Lemon Tea Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/eslemontea.jpg",
+  },
+  {
+    nama: "Es Lemoned Blue Soda Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/eslemonadebluesoda.jpg",
+  },
+  {
+    nama: "Es Manggo Creamy",
+    harga: "Rp 12.000",
+    gambar: "images/manggocreamy.jpg",
+  },
+  {
+    nama: "Es Manggo Milkshake",
+    harga: "Rp 10.000",
+    gambar: "images/esmanggo.jpg",
+  },
+  {
+    nama: "Es Melon Yakult Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esmelonyakult.jpg",
+  },
+  {
+    nama: "Es Milk Tea Kecil Dan Besar",
+    harga: "Rp 6.000 - 8.000",
+    gambar: "images/esmilktea.jpg",
+  },
+  {
+    nama: "Es Milk Tea coffee Kecil Dan Besar ",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esmilkteakopi.jpg",
+  },
+  {
+    nama: "Es Milk Yakult Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esmilkyakult.jpg",
+  },
+  {
+    nama: "Es Moca Latte coffee",
+    harga: "Rp 8.000",
+    gambar: "images/esmocalatekopi.jpg",
+  },
+  {
+    nama: "Es Mozito Cocopandan Kecil Dan Besar ",
+    harga: "Rp 7.000 - 10.000",
+    gambar: "images/esmozitostrawberry.jpg",
+  },
+  {
+    nama: "Es Mozito melon Kecil Dan Besar",
+    harga: "Rp 7.000 - 10.000",
+    gambar: "images/esmozitomelon.jpg",
+  },
+  {
+    nama: "Es Mozito Orange Kecil Dan Besar",
+    harga: "Rp 7.000 - 10.000",
+    gambar: "images/esmozitoorange.jpg",
+  },
+  {
+    nama: "Es Mozito Strawberry Kecil Dan Besar",
+    harga: "Rp 7.000 - 10.000",
+    gambar: "images/esmozitostrawberry.jpg",
+  },
+  {
+    nama: "Es Orange Yakult Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esorangeyakult.jpg",
+  },
+  {
+    nama: "Es Soda Sirsak Kecil Dan besar",
+    harga: "Rp 6.000 - 8.000",
+    gambar: "images/esmozitosirsak.jpg",
+  },
+  {
+    nama: "Es Susu Kecil Dan Besar",
+    harga: "Rp 7.000 - 10.000",
+    gambar: "images/essusu.jpg",
+  },
+  {
+    nama: "Es Soda Gembira",
+    harga: "Rp 8.000",
+    gambar: "images/essodagembira.jpg",
+  },
+  {
+    nama: "Es Strawberry Milkshake",
+    harga: "Rp 10.000",
+    gambar: "images/esstrawberrymilsake.jpg",
+  },
+  {
+    nama: "Es Strawberry Macha Latte Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esstrawberrymachalatte.jpg",
+  },
+  {
+    nama: "Es Strawberry Latte Matcha",
+    harga: "Rp 8.000",
+    gambar: "images/esstrawberrylattemacha.jpg",
+  },
+  {
+    nama: "Es Strawberry Yakult Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esstrawberryyakult.jpg",
+  },
+  {
+    nama: "Es Susu Yakult Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/essusuyakult.jpg",
+  },
+  {
+    nama: "Es Teh Kecil Dan Besar",
+    harga: "Rp 3.000 - 4.000",
+    gambar: "images/esteh.jpg",
+  },
+  {
+    nama: "Es Teler Kecil Dan Besar",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esteller.jpg",
+  },
+  {
+    nama: "Es Vanilla Latte Coffee",
+    harga: "Rp 8.000 - 10.000",
+    gambar: "images/esvanillalattecoffee.jpg",
+  },
+  {
+    nama: "Green Tea/Matcha Kecil Dan Besar",
+    harga: "Rp 8 .000 - 10.000",
+    gambar: "images/esgreentea.jpg",
+  },
+];
+
+function tampilkanMenu(menu, targetId) {
+  const container = document.getElementById(targetId);
+  menu.forEach((item) => {
+    const div = document.createElement("div");
+    div.className = "menu-item";
+    div.innerHTML = `
+      <img src="${item.gambar}" alt="${item.nama}" class="menu-gambar" />
+      <div class="menu-info">
+        <h3>${item.nama}</h3>
+        <p>${item.harga}</p>
+      </div>
+    `;
+    container.appendChild(div);
+  });
+}
+
+tampilkanMenu(makanan, "makanan");
+tampilkanMenu(minuman, "minuman");
